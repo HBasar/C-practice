a007d306d4f8926c7cb6c7ed8eb7227e0017ac51
habibubasar <105973344+HBasar@users.noreply.github.com>
2022-10-10 02:27:49 +0600

    Add files via upload

--- Data structure Lab/Binary search-B/Binary search-B/main.c
+++ Data structure Lab/Binary search-B/Binary search-B/main.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 int main() {
-    int arr[]={1,2,3,4,5,6,7,8,9,10};
-    int size=9,left=0,right=size,middle,value=8;
+    int arr[]={1,3,4,5,6,7,8,9,10};
+    int size=8,left=0,right=size,middle,value=5;
     while (left<=right) {
         middle=(left+right)/2;
         if(arr[middle]==value){
-            printf("The value is found at: %d number position\n",arr[middle]);
+            printf("The value is found at: %d number position\n",middle+1);
             return 0;
         }else if(arr[middle]<value){
             left=middle+1;
